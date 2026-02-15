e2ed1dbf609693093f4ae652bb4154ca41a42f79
Brayan Sullca <bsullca_16_1995@hotmail.com>
2026-02-15 17:53:20 -0500

    change yml existing cluster execution parameters v2

---
+++ proceso/Preparacion_Ambiente.sql
@@ -0,0 +1,33 @@
+-- Databricks notebook source
+%python
+dbutils.widgets.removeAll()
+
+-- COMMAND ----------
+
+create widget text storageName default "adlsmartdata2810";
+
+-- COMMAND ----------
+
+DROP CATALOG IF EXISTS catalog_smartdata CASCADE;
+
+-- COMMAND ----------
+
+CREATE CATALOG IF NOT EXISTS catalog_smartdata;
+
+-- COMMAND ----------
+
+CREATE SCHEMA IF NOT EXISTS catalog_smartdata.raw;
+CREATE SCHEMA IF NOT EXISTS catalog_smartdata.bronze;
+CREATE SCHEMA IF NOT EXISTS catalog_smartdata.silver;
+CREATE SCHEMA IF NOT EXISTS catalog_smartdata.golden;
+CREATE SCHEMA IF NOT EXISTS catalog_smartdata.exploratory;
+
+CREATE VOLUME IF NOT EXISTS catalog_smartdata.raw.datasets;
+
+-- COMMAND ----------
+
+DESCRIBE VOLUME catalog_smartdata.raw.datasets
+
+-- COMMAND ----------
+
+show volumes in catalog_smartdata.raw;
